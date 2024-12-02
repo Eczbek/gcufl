102369042b33203a1f7f665c12b44608f6c22421
Eczbek <eczbek.void@gmail.com>
2024-12-02 13:51:51 -0500

    Fix naming of `XIEITE_LIFT()` helpers

--- include/xieite/pp/lift.hpp
+++ include/xieite/pp/lift.hpp
@@ -5,7 +5,7 @@
 #define XIEITE_LIFT(...) XIEITE_DETAIL_LIFT(, ..., __VA_ARGS__)
 #define XIEITE_LIFT_LOCAL(...) XIEITE_DETAIL_LIFT(&, ..., __VA_ARGS__)
 #define XIEITE_LIFT_ONE(...) XIEITE_DETAIL_LIFT(,, __VA_ARGS__)
-#define XIEITE_LIFT_LOCAL_ONE(...) XIEITE_DETAIL_LIFT(&,, __VA_ARGS__)
+#define XIEITE_LIFT_ONE_LOCAL(...) XIEITE_DETAIL_LIFT(&,, __VA_ARGS__)
 
 // Temporarily avoiding `XIEITE_ARROW()` until it is fixed
 #define XIEITE_DETAIL_LIFT(capture, ellipsis, ...) \
